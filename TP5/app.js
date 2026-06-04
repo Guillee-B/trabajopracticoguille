391d8d3f8857e3424ad4122bea19325252daec66
Guillee-B <50525498@ivt.edu.ar>
2026-06-04 08:56:48 -0300

    información sobre un alumno

--- TP5/app.js
+++ TP5/app.js
@@ -4,9 +4,13 @@ let suma= numero1 + numero2
 let resta= numero1 - numero2
 let multiplicacion= numero1*numero2
 let division = numero1 / numero2 
+let nombreAlumno = 'Florencia'
+let apellidoAlumno = 'Perez'
+let edadAlumno = 17
 
 
 console.log("El resultado de la suma es:" +suma )
 console.log("El resultado de la resta es:"+resta)
 console.log("El resultado de la multiplicacion es:"+multiplicacion )
 console.log("El resultado de la division es:"+division)
+console.log( "El alumno es " +  nombreAlumno + ' '  +  apellidoAlumno + " tiene "+ edadAlumno+ " años")
