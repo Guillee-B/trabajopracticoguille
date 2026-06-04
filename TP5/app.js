42181f9ca26b429b86b9c41c077effbf42ef06f7
Guillee-B <50525498@ivt.edu.ar>
2026-06-04 08:03:11 -0300

    variables ejercicio 1

--- TP5/app.js
+++ TP5/app.js
@@ -1,7 +1,9 @@
-let numero1 = 12;
-let numero2 = 16;
-let resultado 
+let numero1= 12
+let numero2= 16
+let suma= numero1 + numero2
+let resta= numero1 - numero2
+let multiplicacion= numero1*numero2
+let division = numero1 / numero2 
 
-resultado = numero1 + numero2;
 
 console.log(resultado);
