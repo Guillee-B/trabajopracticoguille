1f9fd6972143d26a42d2f673a76d475fa48fc411
Guillee-B <50525498@ivt.edu.ar>
2026-06-04 09:03:09 -0300

    precio total

--- TP5/app.js
+++ TP5/app.js
@@ -7,6 +7,10 @@ let division = numero1 / numero2
 let nombreAlumno = 'Florencia'
 let apellidoAlumno = 'Perez'
 let edadAlumno = 17
+let precio1= 1500
+let precio2= 2300
+let precio3= 900 
+let total= precio1 + precio2 + precio3
 
 
 console.log("El resultado de la suma es:" +suma )
@@ -14,3 +18,4 @@ console.log("El resultado de la resta es:"+resta)
 console.log("El resultado de la multiplicacion es:"+multiplicacion )
 console.log("El resultado de la division es:"+division)
 console.log( "El alumno es " +  nombreAlumno + ' '  +  apellidoAlumno + " tiene "+ edadAlumno+ " años")
+console.log(" El precio total es:"+total)
