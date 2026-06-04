42d3e9d010f64419dd6d4da186ab5892756f23d2
Guillee-B <50525498@ivt.edu.ar>
2026-06-04 09:14:38 -0300

    4 de junio

--- TP5/app.js
+++ TP5/app.js
@@ -11,6 +11,9 @@ let precio1= 1500
 let precio2= 2300
 let precio3= 900 
 let total= precio1 + precio2 + precio3
+let comidaFavorita= "Empanadas de carne"
+let colorFavorito= "Rojo"
+let numeroFavorito= 15
 
 
 console.log("El resultado de la suma es:" +suma )
@@ -19,3 +22,4 @@ console.log("El resultado de la multiplicacion es:"+multiplicacion )
 console.log("El resultado de la division es:"+division)
 console.log( "El alumno es " +  nombreAlumno + ' '  +  apellidoAlumno + " tiene "+ edadAlumno+ " años")
 console.log(" El precio total es:"+total)
+console.log("Mi comida favorita es" + comidaFavorita + " ," + " me gusta mucho el color"  +  colorFavorito + "y mi numero favorito es" + numeroFavorito )
