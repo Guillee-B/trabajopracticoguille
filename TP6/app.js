49912d4c8fad14e419bcd23ba3cb5bef367417c9
alumno <alumno@Instituto.local>
2026-07-02 09:16:05 -0300

    tp 6

--- TP6/app.js
+++ TP6/app.js
@@ -1,2 +1,42 @@
-let titulo = document.querySelector ("h1")
-let body = d
+
+
+// Variables del desafio
+//let Tamaño = document.querySelector ("tamaño")
+//let Reiniciar = document.querySelector ("reiniciar")
+
+// Estado original de las variables
+/*let TextoOriginal = parrafo.textContent
+let ColorOriginal = black
+let FondoOriginal = palevioletred
+let LetraOriginal = Georgia, 'Times New Roman', Times, serif
+let TamañoOriginal = 24px 
+*/
+
+let btnfondo = document.querySelector ("#btnFondo")
+btnfondo.onclick = function () {
+    Parrafo.style.backgroundColor = 'blue'
+}
+
+let btntexto = document.querySelector ("#btnTexto")
+btntexto.onclick = function () {
+    Parrafo.textContent = " Buen dia a todos"
+}
+
+let btnletra = document.querySelector ("#btnLetra")
+btnletra.onclick = function () {
+    Parrafo.style.color = " white"
+}
+
+let tipoletra = document.querySelector ( "#btnTipoLetra")
+btntipoletra.onclick = function () {
+    Parrafo.style.fontFamily = "Arial, Helvetica, sans-serif"
+}
+
+
+let btntamaño = document.querySelector ("#btnTamaño")
+btntamaño.onclick = function () {
+    Parrafo.style.fontSize  = "24px"
+}
+
+let Reiniciar = document.querySelector ("#btnReinicio")
+
