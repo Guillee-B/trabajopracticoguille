f16df4a6a70b5d6cfc42ff79b8bc6d3150e172e5
Guillee-B <50525498@ivt.edu.ar>
2026-07-30 09:15:24 -0300

    30/7 TP/

---
+++ TP7/app.js
@@ -0,0 +1,9 @@
+let eje1 = document.querySelector("#eje1")
+let botonEje1 = document.querySelector("#botonEje1")
+let edad = 15
+
+botonEje1.onclick = function(){
+    eje1.textContent = "Sos mayor de edad"
+} else {
+  eje1.textContent = "Sos menor de edad"
+}
