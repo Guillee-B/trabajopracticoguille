a2235f0989a04b875be2ae4159fb5607972d022d
alumno <50525498@ivt.edu.ar>
2026-08-13 09:03:06 -0300

    13/8

--- TP7/app.js
+++ TP7/app.js
@@ -1,10 +1,9 @@
 let eje1 = document.querySelector("#eje1")
 let botonEje1 = document.querySelector("#botonEje1")
-let edad = 15
-
+let edad = document.querySelector("#In1")
 
 botonEje1.onclick = function () {
-  if (edad >= 18) {
+  if (edad.value >= 18) {
     eje1.textContent = "Sos mayor de edad"
   } else {
     eje1.textContent = "Sos menor de edad"
@@ -13,10 +12,10 @@ botonEje1.onclick = function () {
 
 let eje2 = document.querySelector("#eje2")
 let botonEje2 = document.querySelector("#botonEje2")
-let NombreUsuario = 'Marcos'
+let NombreUsuario =  document.querySelector("#InputEje2")
 
 botonEje2.onclick = function () {
-  if (NombreUsuario == "Nahuel") {
+   if (NombreUsuario.value == "Nahuel") {
     eje2.textContent = "Bienvenido Nahuel,¿Como estas?"
   } else {
     eje2.textContent = "Bienvenido usuario"
@@ -25,8 +24,11 @@ botonEje2.onclick = function () {
 
 let eje3 = document.querySelector("#eje3")
 let botonEje3 = document.querySelector("#botonEje3")
+let inputEje3 = document.querySelector("#InputEje3")
+
 botonEje3.onclick = function () {
-  if ((NombreUsuario == "Nahuel") || (NombreUsuario == "Marcos")) {
+ 
+  if ((NombreUsuario.value == "Nahuel") || (NombreUsuario.value == "Marcos")) {
     eje3.textContent = "Bienvenido " + NombreUsuario + "¿Como estas?"
   } else {
     eje3.textContent = "Bienvenido usuario"
@@ -35,11 +37,13 @@ botonEje3.onclick = function () {
 
 let eje4 = document.querySelector("#eje4")
 let botonEje4 = document.querySelector("#botonEje4")
-let numero = 145
+let numero = document.querySelector("#InputEje4")
+
+
 botonEje4.onclick = function () {
-  if (numero > 0) {
+  if (numero.value > 0) {
     eje4.textContent = "El numero es positivo"
-   } else if (numero < 0) {
+   } else if (numero.value < 0) {
     eje4.textContent = "El numero es negativo"
    } else {
     eje4.textContent = "El numero es igual a 0"
@@ -49,17 +53,18 @@ botonEje4.onclick = function () {
 
 let eje5 = document.querySelector("#eje5")
 let botonEje5 = document.querySelector("#botonEje5")
-let Edad = 17 
+let Edad =  document.querySelector("#InputEje5")
+
 botonEje5.onclick = function () {
-  if ((Edad >= 6 ) || (Edad <= 11 )) {
+  if ((Edad.value >= 6  && Edad.value <= 11 )) {
     eje5.textContent = "La edad corresponde a un niño"
-  } else if ((Edad >= 12) || (Edad <= 18)) {
+  } else if (Edad.value >= 12 && Edad.value <= 18) {
     eje5.textContent = "La edad corresponde a un adolescente"
-  } else if ((Edad >= 19) || (Edad <= 26)) {
+  } else if (Edad.value >= 19 && Edad.value <= 26) {
     eje5.textContent = "La edad corresponde a un joven"
-  } else if ((Edad >=27) || (Edad <=59)) {
+  } else if (Edad.value >=27 && Edad.value <=59) {
     eje5.textContent = "La edad corresponde a un adulto"
-  } else if (Edad >= 60) {
+  } else if (Edad.value >= 60) {
     eje5.textContent = "La edad corresponde a un anciano"
   } else {
     eje5.textContent = "La edad no se encuentra en el sistema"
@@ -67,22 +72,22 @@ botonEje5.onclick = function () {
 
 }
 
-let eje6 = document.querySelector("#eje6")
-let botonEje6 = document.querySelector("#botonEje6")
-let Dia = viernes
+let eje6 = document.querySelector("#eje6");
+let botonEje6 = document.querySelector("#botonEje6");
+let Dia = document.querySelector("#InputEje6");
 botonEje6.onclick = function () {
-  if (( Dia = lunes) || (Dia = martes) || (Dia = miercoles) || (Dia = jueves) || (Dia = viernes)) {
+  if (( Dia.value == "lunes") || (Dia.value == "martes") || (Dia.value == "miercoles") || (Dia.value == "jueves") || (Dia == "viernes")) {
     eje6.textContent = "Es un dia laborable."
-  } else if (( Dia = sabado) || (Dia = domingo)) {
-    eje6.textContent = "No es un dia laborable."
+  } else if (( Dia.value == "sabado") || (Dia.value == "domingo")) {
+    eje6.textContent = "No es un dia laborable.";
   }
 }
 
 let eje7 = document.querySelector("#eje7")
 let botonEje7 = document.querySelector("#botonEje7")
-let contrasenia = batata
+let contrasenia = document.querySelector("#InputEje7") 
 botonEje7.onclick = function () {
-  if (contrasenia = "secreto") {
+  if (contrasenia.value == "secreto") {
     eje7.textContent = "Acceso concedido."
   } else {
     eje7.textContent = "Acceso denegado."
